7dd130d0f7382722470b3f4e51fc8f1eef05f87c
seforarodriguez <seforarodriguez13@gmail.com>
2016-03-01 13:29:30 -0600

    Changes to associate code in models

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict';;;
+'use strict';
 
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('postgres://localhost:5432/nodegresql');
--- server.js
+++ server.js
@@ -2,15 +2,115 @@
 
 const express = require('express');
 
+const models = require('./models/');
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send({
-  status:'Success'
-  })
-})
-app.listen(PORT, () => {
-  console.log(`App listeting on port ${PORT}`)
+    status: 'Success'
+  });
 });
+
+app.get('/genres', (req, res) => {
+  models.Genre.findAll()
+    .then(genres => res.send(genres));
+});
+
+app.get('/mediatypes', (req, res) => {
+  models.MediaType.findAll()
+    .then(types => res.send(types));
+});
+
+app.get('/artists', (req, res) => {
+  models.Artist.findAll()
+    .then(artists => res.send(artists));
+});
+
+app.get('/playlists', (req, res) => {
+  models.Playlist.findAll()
+    .then(playlists => res.send(playlists));
+});
+
+app.get('/albums', (req, res) => {
+  models.Album.findAll({
+      attributes: ['AlbumId', 'Title'],
+      include: {
+        model: models.Artist,
+        attributes: ['Name']
+      }
+    })
+    .then(albums => res.send(albums));
+});
+
+app.get('/invoices', (req, res) => {
+  models.Invoice.findAll({
+      attributes: { exclude: 'CustomerId' },
+      include: {
+        model: models.Customer,
+        attributes: { exclude: [
+          'CustomerId',
+          'SupportRepId'
+        ]}
+      }
+    })
+    .then(invoices => res.send(invoices));
+});
+
+app.get('/customers', (req, res) => {
+  models.Customer.findAll()
+    .then(customers => res.send(customers));
+});
+
+app.get('/customers/:id', (req, res) => {
+  models.Customer.findOne({
+    where: {
+      CustomerId: req.params.id
+    }
+  }).then(customer => res.send(customer));
+});
+
+app.get('/customers/:id/invoices', (req, res) => {
+  models.Customer.findOne({
+    where: {
+      CustomerId: req.params.id
+    },
+  })
+  .then(customer => customer.getInvoices())
+  .then(invoices => res.send(invoices));
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
